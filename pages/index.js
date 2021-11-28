1741927bb4c134137fb36f48813502b472cb79cb
trevinshu <trevinshu2008@gmail.com>
2021-11-28 16:14:34 -0700

    added layout

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,27 @@
 import { AppBar } from 'components/appbar';
 import { GoogleProvider } from 'components/authentication/providers';
 import { Title } from 'ui/title';
-
+import { Spacer } from 'ui/spacers';
+import { EmailAndPassword } from 'components/authentication/login';
+import { HighLight, Legal } from 'ui/legal';
+import { PageLayout, PageHeader, PageFooter } from 'layouts/loginpage';
 function index() {
   return (
     <>
       <AppBar />
-      <Title>Account Login</Title>
-      <GoogleProvider>With Google</GoogleProvider>
+      <PageLayout>
+        <PageHeader>
+          <Title>Account Login</Title>
+        </PageHeader>
+        <GoogleProvider>With Google</GoogleProvider>
+        <Spacer>OR</Spacer>
+        <EmailAndPassword />
+        <PageFooter>
+          <Legal>
+            Legal Stuff <HighLight>Terms and Conditions</HighLight>
+          </Legal>
+        </PageFooter>
+      </PageLayout>
     </>
   );
 }
