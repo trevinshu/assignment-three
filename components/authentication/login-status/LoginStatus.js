5e9cd150200681fc01e5706f68a8514d7d1a460d
trevinshu <trevinshu2008@gmail.com>
2021-11-28 17:16:35 -0700

    logout

--- components/authentication/login-status/LoginStatus.js
+++ components/authentication/login-status/LoginStatus.js
@@ -1,14 +1,33 @@
- 
-
-import { IoPersonCircleSharp } from "react-icons/io5";
-import {LoginStatus} from './styles'
-
-
-
+import { IoPersonCircleSharp } from 'react-icons/io5';
+import { LoginStatus } from './styles';
+import { useAuth } from 'lib/hooks/useAuth';
+import { auth } from 'lib/firebase';
+import { signOut } from '@firebase/auth';
+import { useRouter } from 'next/dist/client/router';
 function UserLoginStatus({ size, color, status, ...props }) {
+  const user = useAuth();
+  const router = useRouter();
+  function handleClick() {
+    //sign the user out
+    signOut(auth).then(() => {
+      router.push('/');
+    });
+  }
+  if (user) {
+    return (
+      <LoginStatus bgcolor="#d6fecd" onClick={handleClick}>
+        <IoPersonCircleSharp size={size || '1.75rem'} />
+        <figcaption>
+          <p>Status</p>
+          <p>logout</p>
+        </figcaption>
+      </LoginStatus>
+    );
+  }
+
   return (
     <LoginStatus>
-      <IoPersonCircleSharp size={size || "1.75rem"} />
+      <IoPersonCircleSharp size={size || '1.75rem'} />
       <figcaption>
         <p>Status</p>
         <p>please login</p>
--- components/authentication/user-login/UserLogin.js
+++ components/authentication/user-login/UserLogin.js
@@ -1,22 +1,29 @@
- import TextInput from "ui/forms/TextInput"
- import { Button } from "ui/buttons"
-import Login from "./styled"
- 
- 
-function UserLogin ({...props}){
- 
-   
-    return (
-        <>
-        <Login {...props} >    
-         <TextInput label="Email"  id="emailAddress" placeholder="janedoe@home.com" {...props}/>
-         <TextInput label="Password"  type="password" id="emailAddress" placeholder="use a secure password" {...props}/>
-    
-        <Button bgcolor="#ed4747" color="white" noBoxShadow {...props} type="submit">LOGIN</Button>
-        </Login>
-        </>
+import TextInput from 'ui/forms/TextInput';
+import { Button } from 'ui/buttons';
+import { auth } from 'lib/firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { useState } from 'react';
+import Login from './styled';
 
-    )
+function UserLogin({ ...props }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  async function handleClick(e) {
+    e.preventDefault();
+    const isUser = await signInWithEmailAndPassword(auth, email, password);
+    console.log(isUser);
+  }
+  return (
+    <>
+      <Login {...props} onSubmit={(e) => handleClick(e)}>
+        <TextInput label="Email" onChange={(e) => setEmail(e.currentTarget.value)} id="emailAddress" placeholder="janedoe@home.com" {...props} />
+        <TextInput label="Password" onChange={(e) => setPassword(e.currentTarget.value)} type="password" id="emailAddress" placeholder="use a secure password" {...props} />
+        <Button bgcolor="#ed4747" color="white" noBoxShadow {...props} type="submit">
+          LOGIN
+        </Button>
+      </Login>
+    </>
+  );
 }
 
-export default UserLogin
+export default UserLogin;
